e1e3d6da15ceadef72f1b5b799666c1b60152f43
stud <stud@W04-M1MAC130-10.local>
2025-10-20 15:32:34 +0200

    Initial commit

---
+++ TheQuackApp/TheQuackApp/TheQuackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheQuackAppApp.swift
+//  TheQuackApp
+//
+//  Created by stud on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheQuackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
